5cca1cc4714552f733a17e2311ced8b132db5941
Atobas321 <149668611+Atobas321@users.noreply.github.com>
2024-01-22 07:52:58 -0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+              "./src/**/*.{tsx,ts,jsx,js,css}"
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
